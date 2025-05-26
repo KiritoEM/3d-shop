755bfe628ccebbdba4a5b3b0fad8fd2ec4af3b69
Loick Emadisson <johankirito64@gmail.com>
2025-05-26 16:45:25 +0300

    feat(features): add about-cards

--- src/features/landing/components/About.tsx
+++ src/features/landing/components/About.tsx
@@ -1,12 +1,31 @@
+import { ABOUT_DATA } from "@/helpers/data/landing-data";
+import { cn } from "@/lib/utils";
 import { JSX } from "react";
 
 const About = (): JSX.Element => {
   return (
-    <section className="about container px-0 pl-12 min-h-screen">
-      <div className="about__content w-[50%]">
-        <h2 className="font-michroma text-5xl text-foreground">
+    <section className="about container px-0 pl-12 min-h-screen flex justify-end">
+      <div className="content w-full max-w-[600px] relative right-[50px]">
+        <h2 className="content__title font-michroma text-5xl text-foreground">
           Découvrez une nouvelle façon de shopper
         </h2>
+
+        <div className="about-cards flex flex-col space-y-8 mt-16">
+          {ABOUT_DATA.map((item, index) => (
+            <article
+              key={index}
+              className={
+                cn("p-5 yellow-linear flex items-center space-x-8", index % 2 === 0 && "relative left-24")
+              }
+            >
+              <div className="about-cards__icon w-[140px] h-[140px] bg-white rounded-xl"></div>
+
+              <h4 className="about-cards__title text-2xl flex-1">
+                {item.text}
+              </h4>
+            </article>
+          ))}
+        </div>
       </div>
     </section>
   );
---
+++ src/helpers/data/landing-data.ts
@@ -0,0 +1,14 @@
+export const ABOUT_DATA = [
+    {
+        icon: "",
+        text: "Explorez chaque produit sous tous les angles en 3D. "
+    },
+    {
+        icon: "",
+        text: "Paiement sécurisé en un clic. Simple, rapide, sans friction."
+    },
+    {
+        icon: "",
+        text: "Des suggestions  qui collent parfaitement à vos goûts."
+    }
+]
