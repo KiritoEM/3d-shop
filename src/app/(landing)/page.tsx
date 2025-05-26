32b9d552c9c3b63bcb4146cf0ab1c4cd823eba23
Loick Emadisson <johankirito64@gmail.com>
2025-05-26 10:25:11 +0300

    feat(page): add locomotive-scroll to home

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,10 +1,28 @@
+"use client";
+
 import Hero from "@/features/landing/components/Hero";
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
 const Landing = (): JSX.Element => {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+
+      new LocomotiveScroll({
+        // @ts-ignore
+        lenisOptions: {
+          duration: 1.5,
+          easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+          smoothWheel: true,
+        },
+      });
+    })();
+  }, []);
   return (
     <section className="landing">
       <Hero />
+      <div className="h-[200vh]"></div>
     </section>
   );
 };
